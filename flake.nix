092b508e0aba43fec114150eb78487ea3ec94143
Szabo Gergely <gege251@mailbox.org>
2025-02-24 10:22:54 +0100

    Add aarch64 to supported systems

--- flake.nix
+++ flake.nix
@@ -26,6 +26,6 @@
         ./is-plutus-data-derive/build.nix
       ];
       debug = true;
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     };
 }
