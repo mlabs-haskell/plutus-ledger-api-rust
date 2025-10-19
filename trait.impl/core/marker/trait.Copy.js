3ac411f158f99fffb5e1bfca3c414111d02d45d5
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2025-10-19 18:03:09 +0000

    Update gh-pages
    Store path: /nix/store/h0fdxyhdd8h229088xkgfxmpjdph6m7p-plutus-ledger-api-github-pages

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["plutus_ledger_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"plutus_ledger_api/v3/transaction/enum.Vote.html\" title=\"enum plutus_ledger_api::v3::transaction::Vote\">Vote</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"plutus_ledger_api/v1/interval/enum.Extended.html\" title=\"enum plutus_ledger_api::v1::interval::Extended\">Extended</a>&lt;T&gt;<div class=\"where\">where\n    T: FeatureTraits + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[809]}
